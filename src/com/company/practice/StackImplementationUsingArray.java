2d5573559b598dd6845b3cb46d0ec2e26251016a
mukkatharun <tharunmukka1@gmail.com>
2020-05-16 04:56:42 +0530

    Stack implementation using arrays

--- src/com/company/practice/StackImplementationUsingArray.java
+++ src/com/company/practice/StackImplementationUsingArray.java
@@ -1,4 +1,53 @@
 package com.company.practice;
 
 public class StackImplementationUsingArray {
+  int size;
+    int stack[];
+    int top;
+
+    StackImplementationUsingArray(int size) {
+        this.size = size;
+        this.stack = new int[size];
+        this.top = -1;
+    }
+
+    public void push(int value) {
+        if(size -2 != top) {
+            top++;
+            stack[top] = value;
+            System.out.println("Pushed element" + value);
+        }
+        else {
+            System.out.println("Stack is full");
+        }
+    }
+
+    public void pop() {
+        if(top != -1 ) {
+            int topValue = stack[top];
+            top--;
+            System.out.println("the popped element " + topValue);
+        }
+        else {
+            System.out.println("the stack is empty");
+        }
+    }
+
+    public void top() {
+        int topEkement = stack[top];
+        System.out.println("the top element us " + topEkement);
+    }
+
+    public static void main(String args[]) {
+        StackImplementationUsingArray stackImplementationUsingArray = new StackImplementationUsingArray(5);
+        stackImplementationUsingArray.pop();
+        stackImplementationUsingArray.push(5);
+        stackImplementationUsingArray.push(1);
+        stackImplementationUsingArray.push(2);
+        stackImplementationUsingArray.pop();
+        stackImplementationUsingArray.top();
+        stackImplementationUsingArray.push(6);
+        stackImplementationUsingArray.push(4);
+        stackImplementationUsingArray.top();
+    }
 }
